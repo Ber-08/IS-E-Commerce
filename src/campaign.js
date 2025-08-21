2c7f9db0e4fc10fb04c91c2a82920f50d6a8a004
Ber-08 <bezytryphena.com>
2025-08-21 17:16:02 +0530

    page definitions

--- src/campaign.js
+++ src/campaign.js
@@ -1,6 +1,22 @@
 export const onClickCart = () => {
-    if(window.SalesforceInteractions){
-      window.SalesforceInteractions.sendEvent({
-        interaction: { name: "Cart Page" }
-      });
-}}
+  if (window.SalesforceInteractions) {
+    window.SalesforceInteractions.sendEvent({
+      interaction: { name: "Cart Page" },
+    });
+  }
+};
+
+export const onClickHome = () => {
+  if (window.SalesforceInteractions) {
+    window.SalesforceInteractions.sendEvent({
+      interaction: { name: "Home Page" },
+    });
+  }
+};
+export const onClickJournal = () => {
+  if (window.SalesforceInteractions) {
+    window.SalesforceInteractions.sendEvent({
+      interaction: { name: "Journal Page" },
+    });
+  }
+};

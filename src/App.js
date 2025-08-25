ffd8c80696f69d2cab670d29fb29795e5ef6ec6f
Ber-08 <bezytryphena.com>
2025-08-25 11:32:14 +0530

    change

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route
-            path="/home"
+            path="/"
             exact
             element={
               <>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -56,7 +56,7 @@ const NavBar = () => {
     <div>
       <header class="banner" role="banner">
         <nav class="navbar" role="navigation" aria-label="menu">
-          <Link to="/home" onClick={onClickHome}>
+          <Link to="/" onClick={onClickHome}>
             <img src={logo} alt="" className=" ml-32" />
           </Link>
 

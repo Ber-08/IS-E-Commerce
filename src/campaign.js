868d612ef04844f10c5c424dd58c46ebfd95257b
Ber-08 <bezytryphena.com>
2025-09-10 16:01:06 +0530

    addtocart8

--- src/campaign.js
+++ src/campaign.js
@@ -87,30 +87,9 @@ export const onClickOurBestSeller = (item) => {
 
 export const onClickAddToCart = (item) => {
   if (window.SalesforceInteractions) {
-    const lineItem =
-      window.SalesforceInteractions.mcis.buildLineItemFromPageState({
-        sku: item.sku || item.id,
-        name: item.title,
-        price: item.price,
-        currency: item.currency || "USD",
-        quantity: item.quantity || 1,
-      });
-
     window.SalesforceInteractions.sendEvent({
       interaction: {
         name: window.SalesforceInteractions.CartInteractionName.AddToCart,
-        lineItem: lineItem,
-      },
-
-      //optional
-      catalogObject: {
-        type: "Product",
-        id: item.id,
-        attributes: {
-          title: item.title,
-          price: item.price,
-          image: item.image,
-        },
       },
     });
   }
